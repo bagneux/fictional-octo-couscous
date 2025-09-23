93547f5f28aae677a9756be6e14a45b9d74d0c0b
Nikolai Kuzmin <bagneux@mail.ru>
2025-09-23 16:08:39 +0300

    class Main implemented

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,34 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Ivan", "Tsarevich", (byte) 30);
+        userService.saveUser("Rusalka", "Ivanovna", (byte) 25);
+        userService.saveUser("Sergey", "Radonezhsky", (byte) 40);
+        userService.saveUser("Anna", "Ioannovna", (byte) 22);
+
+        List<User> allUsers = userService.getAllUsers();
+        for (User user : allUsers) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+
+
+
     }
 }
